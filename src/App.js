1ef58a4a21126313ee8b480038e85ab017c232d9
brarver <brarver@yahoo.com>
2020-09-21 13:31:04 -0600

    29 refactor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,60 @@
 import React from "react";
-import Counter from "./components/Counter";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import friends from "./friends.json";
+import "./App.css";
 
-function App() {
-  return <Counter />;
+class App extends React.Component {
+
+  state = {
+    friends
+  };
+
+  removeFriend = (id) => {
+     const friends = this.state.friends.filter(friend => {
+       return friend.id !== id
+     })
+
+    //  const friends = this.state.friends.filter(friend => friend.id !== id);
+
+     this.setState({ friends })
+  };
+
+  render() {
+    return (
+      <Wrapper>
+      <h1 className="title">Friends List</h1>
+      {this.state.friends.map(friend => (
+        <FriendCard
+          removeFriend={this.removeFriend}
+          id={friend.id}
+          key={friend.id}
+          name={friend.name}
+          image={friend.image}
+          occupation={friend.occupation}
+          location={friend.location}
+        />
+      ))}
+      </Wrapper>
+    )
+   
+  
+      {/*<FriendCard
+        name={friends[1].name}
+        image={friends[1].image}
+        occupation={friends[1].occupation}
+        location={friends[1].location}
+      />
+      <FriendCard
+        name={friends[2].name}
+        image={friends[2].image}
+        occupation={friends[2].occupation}
+        location={friends[2].location}
+      />*/}
+    
+  };
 }
 
 export default App;
+
+
